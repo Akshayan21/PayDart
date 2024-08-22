7838b1ebed26c9d5646520c637d218815b8c13be
Akshayan21 <99593289+Akshayan21@users.noreply.github.com>
2024-08-22 12:24:52 +0530

    Screens

---
+++ lib/UI_Screens/login_screen.dart
@@ -0,0 +1,148 @@
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pay_dart/UI_Screens/Institutions.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Center(
+                child: Image.asset("assets/Logo_Main.png",
+                    width: screenWidth * 0.5, height: screenHeight * 0.2),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 125, left: 10),
+              child: Text("Hey There!\nLogin To Continue",
+                  style: GoogleFonts.poppins(
+                      fontSize: 30, fontWeight: FontWeight.w500)),
+            ),
+            SizedBox(
+              height: screenHeight * 0.05,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  labelText: 'Username',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.05,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: _toggleVisibility,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.01,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 220.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forget Password?",
+                    style: GoogleFonts.poppins(
+                        fontSize: 15, fontWeight: FontWeight.w400),
+                  ),
+                )),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            SizedBox(
+              width: screenWidth * 0.6,
+              height: screenHeight * 0.06,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 15, 53, 156),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text("Login",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.09),
+            SizedBox(
+              width: screenWidth * 0.8,
+              height: screenHeight * 0.06,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 245, 152, 22),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Institutions()),
+                  );
+                },
+                child: Text("Pay Fees",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:pay_dart/UI_Screens/Dashboard.dart';
+import 'package:pay_dart/UI_Screens/Institutions.dart';
+import 'package:pay_dart/UI_Screens/details_displaying_screen.dart';
+import 'package:pay_dart/UI_Screens/details_fetching_screen.dart';
+import 'package:pay_dart/UI_Screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,17 +12,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 45, 133, 248)),
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: const DetailsDisplayingScreen(),
     );
   }
 }
